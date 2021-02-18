3071a7171a1186c76a3328966b1601d13ad2ef46
Artem Zakharchenko <kettanaito@gmail.com>
2021-02-18 17:03:10 +0100

    Adds tests for per-page routes and custom markup

---
+++ test/suites/pageWith.test.ts
@@ -0,0 +1,58 @@
+import { pageWith } from 'src/index'
+
+test('opens a browser with the given usage example', async () => {
+  const { page } = await pageWith({
+    example: 'test/fixtures/hello.js',
+  })
+
+  const bodyText = await page.textContent('#text')
+
+  expect(bodyText).toBe('hello')
+})
+
+test('supports multiple independent pages', async () => {
+  const first = await pageWith({
+    example: 'test/fixtures/hello.js',
+  })
+
+  const second = await pageWith({
+    example: 'test/fixtures/goodbye.js',
+  })
+
+  expect(await first.page.textContent('#text')).toBe('hello')
+  expect(await second.page.textContent('#text')).toBe('goodbye')
+})
+
+test('supports per-page routes', async () => {
+  const { page, request } = await pageWith({
+    example: 'test/fixtures/hello.js',
+    routes(app) {
+      app.get('/user', (req, res) => {
+        res.json({ firstName: 'John' })
+      })
+    },
+  })
+
+  const res = await request('/user')
+  expect(res.status()).toBe(200)
+  expect(await res.json()).toEqual({ firstName: 'John' })
+
+  // Close the page to remove its routes.
+  await page.close()
+
+  const second = await pageWith({
+    example: 'test/fixtures/hello.js',
+  })
+  const secondResponse = await second.request('/user')
+  expect(secondResponse.status()).toBe(404)
+})
+
+test('supports custom markup', async () => {
+  const { page } = await pageWith({
+    example: 'test/fixtures/hello.js',
+    markup: 'test/fixtures/custom.html',
+  })
+
+  const divContent = await page.textContent('#app')
+  expect(divContent).toBe('Custom markup')
+})
