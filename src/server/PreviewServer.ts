2c73e28ffdd5a91b95ba3dc83cd880c1f5096d3c
Miles Richardson <miles@splitgraph.com>
2022-08-29 19:39:34 +0100

    feat(PreviewServer): return address family as part of `connectionInfo` object
    This is not strictly necessary for any bugfix, but it will make
    life easier for code that consumes the connectionInfo object,
    which would otherwise need to parse `connectionInfo.address`
    itself and/or assume that any address containing `:` is an IPv6
    address (which is probably a valid assumption, albeit not a robust one).

--- src/server/PreviewServer.ts
+++ src/server/PreviewServer.ts
@@ -24,6 +24,7 @@ interface ServerConnectionInfo {
   port: number
   host: string
   url: string
+  family: AddressInfo['family']
 }
 
 interface CacheEntry {
@@ -118,6 +119,7 @@ export class PreviewServer {
           port,
           host: address,
           url,
+          family,
         }
         this.connection = connection
         this.log('preview server established at %s', url)
